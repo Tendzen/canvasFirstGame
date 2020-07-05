37e580fbb17944280fe7a0eeb09bf397fdb1cdb7
Pavel Ekishev <48602903+Tendzen@users.noreply.github.com>
2020-07-05 15:17:48 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 
 const audioPlayer = document.getElementById("audioPlayer");
 const playlist = new Array('musicForWebGame/doomBFG.mp3', 'musicForWebGame/hyper.mp3', 'musicForWebGame/Quixotic.mp3', 'musicForWebGame/singularity.mp3');
-
+audioPlayer.src = playlist[0];
 function playSong(){
 const form = document.forms.demo;
 const radios = form.elements.music;
@@ -47,8 +47,7 @@ let player = {
     __proto__: controlledObject,
     score:[]
 };
-var myMusic;
-var mySound;
+let mySound;
 
 
 function createGame(){
@@ -61,6 +60,42 @@ function createGame(){
     myGameZone.start();
     mySound.play();
     updateGameZone();
+    setInterval(upVolume,1000);
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.volume = 0.1;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    let volumeNow = 0.05;
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+    this.startUp = function(){
+        volumeNow = 0.05;
+        this.sound.volume += volumeNow;
+
+    }
+    this.stopUp = function(){
+        volumeNow = 0.0;
+        if(this.sound.volume > 0.6){
+            this.sound.volume = 0.5;
+        }
+
+    }
+    this.startLower = function(){
+        this.sound.volume -=0.1;
+    }
+    this.stopLower = function(){
+        this.sound.volume == this.sound.volume;
+    }
 
 }
 
@@ -96,90 +131,7 @@ let myGameZone = {
     }
 };  
 
-// class component {
-//     constructor(width, height, color, x, y, type) {
-//         this.type = type;
-//         this.width = width;
-//         this.height = height;
-//         this.speedX = 0;
-//         this.speedY = 0; 
-//         this.x = x;
-//         this.y = y;
-//         this.color = color;
-//     }  
-//     update() {
-//             if (this.type == "text") {
-//                 myGameZone.context.font = this.width + " " + this.height;
-//                 myGameZone.context.fillStyle = this.color;
-//                 myGameZone.context.fillText(this.text, this.x, this.y);
-//             }
-//             else {
-//                 myGameZone.context.fillStyle = this.color;
-//                 myGameZone.context.fillRect(this.x, this.y, this.width, this.height);
-//             }
-//         }
-//     newPosition() { 
-            
-//         if(this.x >= 375 && this.y <= 75){
-//             this.speedX = -this.speedX;
-//             this.speedY = this.speedY;
-//             }
-//         if(this.y <= 90 && this.x >= 400){
-//             this.speedY = -this.speedY;
-//             this.speedX = this.speedX;
-//             }
-//         if(this.y < 0){
-//             this.y = 0;
-//             this.speedY = -this.speedY;
-//         }
-//         if(this.x < 0){
-//             this.x = 0;
-//             this.speedX = -this.speedX;
-//         }
-//         var rockright = myGameZone.canvas.width - this.width;
-//         if(this.x > rockright){
-//             this.x = rockright;
-//             this.speedX = - this.speedX;
-//         }
-//         var rockbottom = myGameZone.canvas.height - this.height;
-//         if(this.y > rockbottom){
-//             console.log(rockbottom);
-//             this.y = rockbottom;
-//             this.speedY = - this.speedY;
-//         }
-//         this.x += this.speedX;
-//         this.y += this.speedY;
-//     }
-//     clicked(){
-
-//         var myleft = this.x;
-//         var myright = this.x + (this.width);
-//         var mytop = this.y;
-//         var myText = this.y + (this.height);
-//         var clicked = true;
-//         if ((myText < myGameZone.y) || (mytop > myGameZone.y) || (myright < myGameZone.x) || (myleft > myGameZone.x)){
-//             clicked = false;
-//         }
-//         return clicked;
-//     }
-   
-//     eatNote(){
-//         var myLeft = this.x;
-//         var myRight = this.x + (this.width);
-//         var myTop = this.y;
-//         var myBottom = this.y + (this.height);
-//         var otherLeft = note.x;
-//         var otherRight = note.x + (note.width);
-//         var otherTop = note.y;
-//         var otherBottom = note.y + (note.height);
-//         var eated = true; 
-//         if ((myBottom < otherTop) || (myTop > otherBottom) || (myRight < otherLeft) || (myLeft > otherRight)) {
-//             eated = false;
-//         }
-//         return eated;
-
-//     }
-//     }
+let eated = false;
 
 function component (width, height, color, x, y, type){
         this.type = type;
@@ -226,32 +178,37 @@ function component (width, height, color, x, y, type){
         this.y += this.speedY;
     }
     this.clicked = function(){
-        var left = 870;
-        var right = 1000;
-        var top = 130;
-        var bottom = 160;
-        var clicked = true;
+        let left = 870;
+        let right = 1000;
+        let top = 130;
+        let bottom = 160;
+        let clicked = true;
         if ((bottom < myGameZone.y) || (top > myGameZone.y) || (right < myGameZone.x) || (left > myGameZone.x)){
             clicked = false;
         }
         return clicked;
+        
     }
-   
-    this.eatNote = function(otherObj){
-        var myLeft = this.x;
-        var myRight = this.x + (this.width);
-        var myTop = this.y;
-        var myBottom = this.y + (this.height);
-        var otherLeft = otherObj.x;
-        var otherRight = otherObj.x + (otherObj.width);
-        var otherTop = otherObj.y;
-        var otherBottom = otherObj.y + (otherObj.height);
-        var eated = true; 
+
+    this.isOnNote = function(otherObj){
+        let myLeft = this.x;
+        let myRight = this.x + (this.width);
+        let myTop = this.y;
+        let myBottom = this.y + (this.height);
+        let otherLeft = otherObj.x;
+        let otherRight = otherObj.x + (otherObj.width);
+        let otherTop = otherObj.y;
+        let otherBottom = otherObj.y + (otherObj.height);
         if ((myBottom < otherTop) || (myTop > otherBottom) || (myRight < otherLeft) || (myLeft > otherRight)) {
             eated = false;
         }
+        else{
+            eated = true;
+        }
         return eated;
+        
     }
+
     this.deleteComponent = function(width, height, color, x, y, type){
         this.width = width;
         this.height = height;
@@ -262,25 +219,9 @@ function component (width, height, color, x, y, type){
         myGameZone.context.clearRect(this.x,this.y,this.width,this.height,this.type);
     }
     }
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.volume = "0.4";
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        setTimeout(this.sound.play(),120);
-        }
-    this.stop = function(){
-        this.sound.pause();
-    }
-    this.upVolume = function(){
-        this.sound.volume +=0.05;
-    }    
-}
-function updateGameZone() { 
+
+function updateGameZone() {
+    console.log(eated);
     let eatednote=0;
     if(myGameZone.x && myGameZone.y){
         if(exit.clicked()){
@@ -288,14 +229,17 @@ function updateGameZone() {
             note = [];
             myGameZone.stop(); 
             myGameZone.canvas.remove();
-            audioPlayer.src ="";
+            mySound.sound.src = "";
             document.getElementById("container").style.bottom ="0px";                 
         }
-    }
+    } 
     for (let i = 0; i < note.length; i++){
-        if(player.eatNote(note[i])){
+        if(player.isOnNote(note[i])){
             note[i].deleteComponent();
             eatednote++;
+            eated = false;
+            console.log(mySound.sound.volume);
+
         }
     }
     myGameZone.clear();
@@ -303,7 +247,7 @@ function updateGameZone() {
     if(myGameZone.frameNo == 2 || everyinterval(150)){
         let minY,minX;
         minX = Math.floor(Math.random() * (myGameZone.canvas.width - 50));
-        minY = Math.floor(Math.random() * (myGameZone.canvas.height - 50));
+        minY = Math.floor(Math.random() * (myGameZone.canvas.height - 80));
         note.push(new component (30,30, "green",minX, minY,"object"));
     }
     for (let i = 0; i < note.length; i++){
@@ -315,18 +259,24 @@ function updateGameZone() {
     scoreName.update();
     player.newPosition();
     player.update();
+    
 }
-
 
 function everyinterval(n) {
     if ((myGameZone.frameNo / n) % 1 == 0) {return true;}
     return false;
 }
+function upVolume(){
+    console.log(eated);
+    if(eated = true){
+        console.log(eated);
+    }
+}
 
 document.onkeydown = checkKey;
 function checkKey(e) {
     e = e || window.event;
-    if(e.keyCode == '38' || e.keyCode == '40' || e.keyCode == '37' || e.keyCode == '39'){
+    if(e.keyCode == '38' || e.keyCode == '40' || e.keyCode == '37' || e.keyCode == '39' || e.keyCode == '32'){
     e.preventDefault ? e.preventDefault() : (e.returnValue = false);
     }
     if (e.keyCode == '38') {
@@ -347,10 +297,27 @@ function checkKey(e) {
     }
 }
 
-let timerId = setTimeout(function lowerVolume(){
-    audioPlayer.volume /= 2;
-    timerId = setTimeout(lowerVolume,6000);
-},2000);
+function getPosition(e) {
+    var posx = 0;
+    var posy = 0;
+  
+    if (!e) var e = window.event;
+  
+      posx = e.pageX;
+      posy = e.pageY;
+    
+  
+    return {
+      x: posx,
+      y: posy
+    }
+  }
+  document.addEventListener( "click", function(e) {
+    var x = getPosition(e).x;
+    var y = getPosition(e).y;
+    console.log("x pos: "+ x +" // y pos:"+ y);
+  });
+
 
 
 
